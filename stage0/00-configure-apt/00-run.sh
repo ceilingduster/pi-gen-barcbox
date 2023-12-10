cfe8a31399c6991141e8f900a56ecdec7796fe62
Ian Redden <razorjack@gmail.com>
2023-12-10 21:41:46 +0000

    added asl and dvswitch repos to stage0

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -2,6 +2,8 @@
 
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/allstarlink.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/dvswitch.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/raspi.list"
 
@@ -14,8 +16,15 @@ fi
 
 cat files/raspberrypi.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg"
 install -m 644 "${STAGE_WORK_DIR}/raspberrypi-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+cat files/dvswitch.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/dvswitch-archive-stable.gpg"
+install -m 644 "${STAGE_WORK_DIR}/dvswitch-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
+cat files/allstarlink.gpg.key | gpg --dearmor > "${STAGE_WORK_DIR}/allstarlink-archive-stable.gpg"
+install -m 644 "${STAGE_WORK_DIR}/allstarlink-archive-stable.gpg" "${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
+
 on_chroot << EOF
-dpkg --add-architecture arm64
+dpkg --add-architecture armhf
 apt-get update
 apt-get dist-upgrade -y
 EOF
